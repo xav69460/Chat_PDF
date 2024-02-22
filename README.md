b5b19f8e2b17bec8b4585a2cf629ecac7d17a78a
DennisChangach <kogeidennis@gmail.com>
2024-02-22 16:10:24 +0300

    Added new Files

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Works best with python version greater >=3.10
 
 
 
+Deployment with Docker & Github Actions
 
 
 References: Krish Naik: https://www.youtube.com/watch?v=uus5eLz6smA
--- app.py
+++ app.py
@@ -81,9 +81,9 @@ def user_input(user_question):
 #Main Function
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.header("Chat with your PDF Content")
 
-    user_question = st.text_input("Ask a Question from the PDF Files")
+    user_question = st.text_input("Ask a Question from your PDF Files")
 
     if user_question:
         user_input(user_question)
